287db6720ef04c90bead5e6a37a30c7a2c7b75c5
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-11-21 21:20:32 -0500

    remove escape key event. Not needed

--- main.js
+++ main.js
@@ -8,13 +8,3 @@ testEl.addEventListener("click", function () {
     // blackScreenEl.classList.remove("d-none");
     // blackScreenEl.style.display = "block";
 });
-document.addEventListener("keydown", function (event) {
-    const key = event.key;
-    if (key === "Escape") {
-        bodyEl.exitFullscreen();
-        blackScreenEl.style.display = "none";
-        console.log("Escape clicked");
-    }
-})
-exitFullscreen
-exitFullscreen
