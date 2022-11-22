02f10c7a5b9a2be3bb6353499ac5d4b47e501671
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-11-21 21:39:59 -0500

    remove bodyEl

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict";
 const testEl = document.getElementById("btn");
-const bodyEl = document.documentElement;
+// const bodyEl = document.documentElement;
 const blackScreenEl = document.getElementById('black-screen');
 
 testEl.addEventListener("click", function () {
     blackScreenEl.requestFullscreen();
-    // blackScreenEl.classList.remove("d-none");
-    // blackScreenEl.style.display = "block";
 });
-bodyEl.addEventListener("keypress", function (event) {
+document.addEventListener("keypress", function (event) {
     if (event.key === "Enter") blackScreenEl.requestFullscreen();
-    // blackScreenEl.classList.remove("d-none");
-    // blackScreenEl.style.display = "block";
+});
+document.addEventListener("keypress", function (event) {
+    console.log("You pressed " + event.key);
 });
